dbf6d955caa77718f2dd209fa3b0fbf1e1423943
esinell <loloshuno@gmail.com>
2023-04-04 13:48:42 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,15 +8,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-  size_t num = 0;
-  const listint_t *current;
+size_t num = 0;
+const listint_t *current;
 
-  for (current = h; current; current = current->next)
-  {
-    printf("%d\n", current->n);
-    num++;
-  }
+	for (current = h; current; current = current->next)
+{
+printf("%d\n", current->n);
+num++;
+}
 
-  return (num);
+	return (num);
 }
 
